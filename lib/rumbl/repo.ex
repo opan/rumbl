2c8b8ccc7d79c753d11dd78ab07db966ee43ede7
opanmustopah <opan.neutron@gmail.com>
2016-09-27 16:32:38 +0700

    Create user migration Add show and new action

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -3,24 +3,24 @@ defmodule Rumbl.Repo do
   In memory repository
   """
 
-  # use Ecto.Repo, otp_app: :rumbl
+  use Ecto.Repo, otp_app: :rumbl
 
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "opanmustopah", password: "opanmustopah"},
-      %Rumbl.User{id: "2", name: "giosakti", password: "giosakti"}
-    ]
-  end
-  def all(_module), do: []
-
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id  end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val  end)
-    end
-  end
+  # def all(Rumbl.User) do
+  #   [
+  #     %Rumbl.User{id: "1", name: "opanmustopah", password: "opanmustopah"},
+  #     %Rumbl.User{id: "2", name: "giosakti", password: "giosakti"}
+  #   ]
+  # end
+  # def all(_module), do: []
+  #
+  # def get(module, id) do
+  #   Enum.find all(module), fn map -> map.id == id  end
+  # end
+  #
+  # def get_by(module, params) do
+  #   Enum.find all(module), fn map ->
+  #     Enum.all?(params, fn {key, val} -> Map.get(map, key) == val  end)
+  #   end
+  # end
 
 end
