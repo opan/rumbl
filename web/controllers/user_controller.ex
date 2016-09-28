bd7976724af64ed5fc4cb45248e876e98879c6e1
opanmustopah <opan.neutron@gmail.com>
2016-09-28 10:53:06 +0700

    Adding authentication using custom plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,10 +1,11 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
 
   alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all(Rumbl.User)
+    users = Repo.all(User)
     render conn, "index.html", users: users
   end
 
@@ -19,7 +20,7 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, user} ->
@@ -31,5 +32,17 @@ defmodule Rumbl.UserController do
     end
 
 
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+
+    end
   end
 end
